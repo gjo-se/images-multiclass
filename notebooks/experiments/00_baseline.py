5903f7ef5726b40a2c5b1d1895a6c457530744cb
Gregory Jo Erdmann <gregory.erdmann@entrados.de>
2026-01-29 11:36:49 +0100

    added Setup

--- notebooks/experiments/00_baseline.py
+++ notebooks/experiments/00_baseline.py
@@ -13,100 +13,71 @@
 #     name: python3
 # ---
 
-# %% md
-# Einleitung
-Ziel des Notebooks und des Experiments
-Kontext und Motivation
+# %% [markdown]
+# # Einleitung
+# - Ziel des Notebooks und des Experiments Kontext und Motivation
+# - ganz zum Schluss per ChatGPT erstellen
+#
 
-# %% md
-# Setup: Lokale Umgebung & Google Colab
-Dieses Notebook ist sowohl für die lokale Entwicklung (z.B. PyCharm) als auch für Google Colab ausgelegt.
+# %% [markdown]
+# # TODO: Link Colab anpassen
+# <a href="https://colab.research.google.com/github/gjo-se/images-multiclass/blob/master/notebooks/experiments/00_baseline.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+#
 
-**Colab-Setup:**
-```python
-# Ausführen, wenn du in Google Colab arbeitest:
-try:
-    import google.colab
-    IN_COLAB = True
-except ImportError:
-    IN_COLAB = False
-
-if IN_COLAB:
-    from google.colab import drive
-    drive.mount('/content/drive')
-    # !pip install -r /content/drive/MyDrive/PFAD/ZU/requirements.txt
-```
-
-**Lokale Umgebung:**
-- Stelle sicher, dass alle Abhängigkeiten aus `requirements.txt` installiert sind.
-- Passe ggf. die Datenpfade an.
-
-# %% md
-# Setup und Imports
-Import aller benötigten Bibliotheken
-Setzen von Zufallsseeds für Reproduzierbarkeit
-Überprüfen der Hardware (z. B. GPU-Verfügbarkeit)
+# %% [markdown]
+# # Imports & Setup
+#
 
 # %%
-import os
-import random
-import numpy as np
-import tensorflow as tf
+from src.setup import SetupEnvironment
 
-# naja es gibt ne Menge zu tun!
+SetupEnvironment();
 
-# Reproduzierbarkeit
-SEED = 42
-os.environ['PYTHONHASHSEED'] = str(SEED)
-random.seed(SEED)
-np.random.seed(SEED)
-tf.random.set_seed(SEED)
 
-# Hardware-Check
-print('TensorFlow Version:', tf.__version__)
-print('GPU verfügbar:', tf.config.list_physical_devices('GPU'))
+# %% [markdown]
+# # Datenvorbereitung
+# Laden und Vorverarbeiten der Daten
+# Explorative Datenanalyse (EDA) mit Visualisierungen
+# Aufteilen in Trainings-, Validierungs- und Testdaten
+#
 
-# Beispiel: Import von Hilfsfunktionen aus src/
-# from src.data import load_data
-# from src.model import build_model
+# %% [markdown]
+# # Modellierung
+# Definition und Visualisierung des Modells
+# Kompilieren des Modells (Loss, Optimizer, Metriken)
+#
 
-# %% md
-# Datenvorbereitung
-Laden und Vorverarbeiten der Daten
-Explorative Datenanalyse (EDA) mit Visualisierungen
-Aufteilen in Trainings-, Validierungs- und Testdaten
+# %% [markdown]
+# # Training
+# Training des Modells mit Trainingsdaten
+# Visualisierung des Trainingsverlaufs (Loss, Accuracy)
+#
 
-# %% md
-# Modellierung
-Definition und Visualisierung des Modells
-Kompilieren des Modells (Loss, Optimizer, Metriken)
+# %% [markdown]
+# # Evaluation
+# Bewertung des Modells auf Validierungs- und Testdaten
+# Darstellung von Metriken und ggf. Confusion Matrix
+#
 
-# %% md
-# Training
-Training des Modells mit Trainingsdaten
-Visualisierung des Trainingsverlaufs (Loss, Accuracy)
+# %% [markdown]
+# # Ergebnisse und Interpretation
+# Zusammenfassung der wichtigsten Erkenntnisse
+# Diskussion von Stärken, Schwächen und möglichen Verbesserungen
+#
 
-# %% md
-# Evaluation
-Bewertung des Modells auf Validierungs- und Testdaten
-Darstellung von Metriken und ggf. Confusion Matrix
+# %% [markdown]
+# # Speicherung und Laden von Modellen
+# Speichern des trainierten Modells
+# Laden und Testen des gespeicherten Modells
+#
 
-# %% md
-# Ergebnisse und Interpretation
-Zusammenfassung der wichtigsten Erkenntnisse
-Diskussion von Stärken, Schwächen und möglichen Verbesserungen
+# %% [markdown]
+# # Fazit und Ausblick
+# Kurzes Fazit und mögliche nächste Schritte
+#
 
-# %% md
-# Speicherung und Laden von Modellen
-Speichern des trainierten Modells
-Laden und Testen des gespeicherten Modells
-
-# %% md
-# Fazit und Ausblick
-Kurzes Fazit und mögliche nächste Schritte
-
-# %% md
-# Anhang
-Zusätzliche Visualisierungen, Code-Snippets oder Referenzen
+# %% [markdown]
+# # Anhang
+# Zusätzliche Visualisierungen, Code-Snippets oder Referenzen
 
 # %%
